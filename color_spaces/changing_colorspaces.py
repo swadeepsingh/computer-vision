7ad4cac9d2149337b2454079f901fad370d7579f
Swadeep Singh <swadeepiitkgp@gmail.com>
2018-12-26 20:05:28 +0530

    Create changing_colorspaces.py

---
+++ color_spaces/changing_colorspaces.py
@@ -0,0 +1,27 @@
+# import packages
+import cv2
+
+
+# load sample image
+def load_image():
+    img = cv2.imread("lena.jpg")
+    return img
+
+
+# LAB colorspace
+def covtLAB(img):
+    lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    return lab
+
+
+# YCrCb colorspace
+def covtYCrCb(img):
+    ycrbc = cv2.cvtColor(bright, cv2.COLOR_BGR2YCrCb)
+    return ycrcb
+
+
+# HSV colorspace
+def covtHSV(img):
+    hsv = cv2.cvtColor(bright, cv2.COLOR_BGR2HSV)
+    return hsv
+    
