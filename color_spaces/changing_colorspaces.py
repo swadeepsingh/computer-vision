6cf3152d0a71ee3e7bc9ddaaa460c490e35633cc
Swadeep Singh <swadeepiitkgp@gmail.com>
2018-12-26 20:11:43 +0530

    Update changing_colorspaces.py

--- color_spaces/changing_colorspaces.py
+++ color_spaces/changing_colorspaces.py
@@ -25,3 +25,13 @@ def covtHSV(img):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     return hsv
     
+
+img = load_image()
+lab_img = covtLAB(img)
+ycrbc_img = covtYCrCb(img)
+hsv_img = covtHSV(img)
+cv2.imshow('original image',img)
+cv2.imshow('Lab image',lab_img)
+cv2.imshow('YCrCb image',ycrbc_img)
+cv2.imshow('HSV image',hsv_img)
+cv2.waitKey(0)
