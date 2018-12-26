1a9d5a724632005e8d47efde00ce68d8ee49ed4c
Swadeep Singh <swadeepiitkgp@gmail.com>
2018-12-26 20:18:32 +0530

    Modified the gray image function

--- color_spaces/changing_colorspaces.py
+++ color_spaces/changing_colorspaces.py
@@ -8,10 +8,10 @@ def load_image():
     return img
 
 
-# Gray image
+# Gray colorspace
 def covtGray(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
+    return gray
     
 # LAB colorspace
 def covtLAB(img):
