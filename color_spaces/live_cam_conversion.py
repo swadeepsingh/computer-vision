3185147b2070834c1b46a9d87ddd6c60d3525097
Swadeep Singh <swadeepiitkgp@gmail.com>
2018-12-27 12:10:16 +0530

    Create live_cam_conversion.py

---
+++ color_spaces/live_cam_conversion.py
@@ -0,0 +1,16 @@
+import cv2
+
+# init web cam
+cam = cv2.VideoCapture(0)
+
+# read frame from web cam
+ret, image = cam.read()
+
+# convert image to gray scale
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# show image in seperate window
+cv2.imshow("Bild modifiziert", image)
+
+# wait for key press
+cv2.waitKey(0)
